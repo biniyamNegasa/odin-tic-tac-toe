02c0673ea286bcf855afb650541267f42dbb3bd6
biniyamNegasa <104790161+biniyamNegasa@users.noreply.github.com>
2024-08-09 14:28:42 +0300

    Implement a player class

---
+++ player.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# A class that initializes a player for the game
+class Player
+  attr_reader :symbol, :choices
+
+  def initialize(symbol)
+    @symbol = symbol
+    @choices = []
+  end
+
+  def append(choice)
+    choices.push(choice)
+  end
+end
